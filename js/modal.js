8516fb86399fe775288a9b09e89793508ecba27b
Vladyslav10 <vshapovalov02@gmail.com>
2022-08-13 15:35:44 +0300

    add popup

--- js/modal.js
+++ js/modal.js
@@ -4,7 +4,6 @@
     const popupLinks = document.querySelectorAll(".popup-link");
     const popupCloseIcon = document.querySelectorAll(".close-popup");
     const lockPadding = document.querySelectorAll(".lock-padding");
-  
     let unlock = true;
   
     const timeout = 800;
@@ -63,7 +62,7 @@
   
     function bodyLock() {
       const findPaddingValue = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
-  
+      console.log(findPaddingValue)
       if (lockPadding.length > 0) {
         for (let index = 0; index < lockPadding.length; index++) {
           const el = lockPadding[index];
